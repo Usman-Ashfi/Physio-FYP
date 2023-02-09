948ea1e8506348a903f564ac68d0c4927244a1b9
Usman-Ashfi <uashfaq48@gmail.com>
2023-02-09 22:41:06 +0500

    Website updates.

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -10,7 +10,9 @@ if (window.innerWidth < 768) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-	AOS && AOS.init();
+	if ('AOS' in window) {
+		AOS.init();
+	}
 
 	var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bss-tooltip]'));
 	var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
